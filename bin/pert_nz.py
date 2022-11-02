282e13762281b9d14f8eb8c478165902e00ab064
Davide Sciotti <davidesciotti@gmail.com>
2022-11-02 19:08:43 +0100

    zp_grid has been constructed in an alternative way

--- bin/pert_nz.py
+++ bin/pert_nz.py
@@ -75,13 +75,15 @@ sqrt2pi = np.sqrt(2 * np.pi)
 
 # various parameters for the perturbed photo-z PDF
 N_pert = 20
-rng = np.random.default_rng(seed=42)
+rng = np.random.default_rng()
 nu_pert = rng.uniform(-1, 1, N_pert)
 nu_pert /= np.sum(nu_pert)  # normalize the weights to 1
 zminus_pert = rng.uniform(-0.15, 0.15, N_pert)
 zplus_pert = rng.uniform(-0.15, 0.15, N_pert)
 omega_fid_pert = rng.uniform(0.69, 0.99)
 c_pert = np.ones((N_pert,))
+
+# TODO z_edges[-1] = 2.5?? should it be 4 instead?
 
 
 # a couple simple functions to go from (z_minus, z_plus) to (zparam, sigma)
@@ -108,7 +110,8 @@ sigma_pert = sigma_n(zminus_pert, zplus_pert, sigma_in)
 @njit
 def base_gaussian(z_p, z, nu, c, z_param, sigma):
     """one of the terms used int the sum of gaussians
-    the name zparam is not the best, but I cannot smply sue z as it is a variable in the function
+    the name zparam is not the best, but I cannot smply sue z as it is a variable in the function.
+    note: the weights (nu) are included in the function! this could change in the future.
     """
     result = (nu * c) / (sqrt2pi * sigma * (1 + z)) * np.exp(-0.5 * ((z - c * z_p - z_param) / (sigma * (1 + z))) ** 2)
     return result
@@ -123,19 +126,23 @@ def pph_fid(z_p, z):
     return result
 
 
+@njit
 def pph_pert(z_p, z):
     result = base_gaussian(z_p, z, nu_pert, c_pert, zparam_pert, sigma_pert)
     return np.sum(result)
 
 
+@njit
 def pph_tot(z_p, z, omega_fid=omega_fid_pert):
     return omega_fid * pph_fid(z_p, z) + (1 - omega_fid) * pph_pert(z_p, z)
 
 
+@njit
 def P_out(z_p, z, z_in, z_out):
     return (z_in - z_out) * (2 * z_p - 2 * z + z_in + z_out)  # sigma_in == sigma_out
 
 
+@njit
 def R_out(z_p, z, z_in, z_out, sigma_in, sigma_out):
     print('sum is mising in this function')
     return sigma_in / sigma_out * np.exp(- P_out(z_p, z, z_in, z_out) / (2 * (sigma_in * (1 + z)) ** 2))
@@ -143,82 +150,34 @@ def R_out(z_p, z, z_in, z_out, sigma_in, sigma_out):
 
 @njit
 def n(z):
-    # result = n_gal * (z / z_0) ** 2 * np.exp(-(z / z_0) ** 1.5)
     result = (z / z_0) ** 2 * np.exp(-(z / z_0) ** 1.5)
-    # TODO normalize the distribution or not? which of the above expressions is correct?
-    # result = result*(30/0.4242640687118783) # normalising the distribution?
     return result
-
-
-################################## niz ##############################################
-
-# choose the cut XXX
-# n_i_import = np.genfromtxt("%s/input/Cij-NonLin-eNLA_15gen/niTab-EP10-RB00.dat" %path) # vincenzo (= davide standard, pare)
-# n_i_import = np.genfromtxt(path.parent / "common_data/vincenzo/14may/InputNz/niTab-EP10-RB.dat") # vincenzo, more recent (= davide standard, anzi no!!!!)
-# n_i_import = np.genfromtxt("C:/Users/dscio/Documents/Lavoro/Programmi/Cij_davide/output/WFs_v3_cut/niz_e-19cut.txt") # davide e-20cut
-# n_i_import = np.load("%s/output/WF/WFs_v2/niz.npy" % project_path)  # davide standard
-
-
-# n_i_import_2 = np.genfromtxt("%s/output/WF/%s/niz.txt" %(path, WFs_input_folder)) # davide standard with zcutVincenzo
-
-
-# def n_i_old(z, i):
-#     n_i_interp = interp1d(n_i_import[:, 0], n_i_import[:, i + 1], kind="linear")
-#     result_array = n_i_interp(z)  # z is considered as an array
-#     result = result_array.item()  # otherwise it would be a 0d array
-#     return result
-#
-#
-# z_values_from_nz = n_i_import[:, 0]
-# i_array = np.asarray(range(zbins))
-# n_i_import_cpy = n_i_import.copy()[:, 1:]  # remove redshift column
-
-# note: this is NOT an interpolation in i, which are just the bin indices and will NOT differ from the values 0, 1, 2
-# ... 9. The purpose is just to have a 2D vectorized callable.
-# n_i_new = interp2d(i_array, z_values_from_nz, n_i_import_cpy, kind="linear")
-
-# note: the normalization of n(z) should be unimportant, here I compute a ratio
-# where n(z) is present both at the numerator and denominator!
-# as a function, including (of not) the ie-20 cut
-# def n_i(z, i):
-#     integrand = lambda z_p, z: n(z) * pph(z_p, z)
-#     numerator = quad(integrand, z_minus[i], z_plus[i], args=(z))
-#     denominator = dblquad(integrand, z_min, z_max, z_minus[i], z_plus[i])
-#     #    denominator = nquad(integrand, [[z_minus[i], z_plus[i]],[z_min, z_max]] )
-#     #    return numerator[0]/denominator[0]*3 to have quad(n_i, 0, np.inf = nbar_b/20 = 3)
-#     result = numerator[0] / denominator[0]
-#     # if result < 6e-19: # remove these 2 lines if you don't want the cut
-#     #     result = 0
-#     return result
-
-
-# compute n_i(z) with simpson
 
 
 # define a grid passing through all the z_edges points, to have exact integration limits
 
-"""
-# 500 pts seems to be enough - probably more than what quad uses!
-z_grid_norm = np.linspace(z_edges[0], z_edges[-1], 500)
-def niz_normalization_simps(i):
-    assert type(i) == int, 'i must be an integer'
-    integrand = np.asarray([niz_unnorm(z, i) for z in z_grid_norm])
-    return simps(integrand, z_grid_norm)
-"""
 
 # intantiate a grid for simpson integration which passes through all the bin edges (which are the integration limits!)
-zp_num = 2_000
-zp_num_per_bin = int(zp_num / zbins)
+zp_points = 2_000
+zp_num_per_bin = int(zp_points / zbins)
 zp_grid = np.empty(0)
 zp_bin_grid = np.zeros((zbins, zp_num_per_bin))
 for i in range(zbins):
     zp_bin_grid[i, :] = np.linspace(z_edges[i], z_edges[i + 1], zp_num_per_bin)
 
+# alternative way: define a grid, then include the bin edges in the grid to have exact integration limits
+zp_bin_grid = np.linspace(z_edges[0], z_edges[-1], zp_points)
+zp_bin_grid = np.append(zp_bin_grid, z_edges)  # add bin edges
+zp_bin_grid = np.sort(zp_bin_grid)
+zp_bin_grid = np.unique(zp_bin_grid)  # remove duplicates (first and last edges were already included)
+zp_bin_grid = np.tile(zp_bin_grid, (zbins, 1))  # repeat the grid for each bin (in each row)
+for i in range(zbins):
+    zp_bin_grid[i, :] = np.where(zp_bin_grid[i, :] > z_edges[i], zp_bin_grid[i, :], 0)  # remove all the points below the bin edge
 
-def niz_unnorm_simps(z, i):
+def niz_unnorm_simps(z, i, pph):
     """numerator of Eq. (112) of ISTF, with simpson integration"""
     assert type(i) == int, 'i must be an integer'
-    niz_unnorm_integrand = pph_fid(zp_bin_grid[i, :], z)
+    niz_unnorm_integrand = pph(zp_bin_grid[i, :], z)
     niz_unnorm_integral = simps(y=niz_unnorm_integrand, x=zp_bin_grid[i, :])
     niz_unnorm_integral *= n(z)
     return niz_unnorm_integral
@@ -240,12 +199,18 @@ def niz_normalization(i, niz_unnormalized_func, pph):
 
 
 def niz_normalized(z, i, pph):
+    """this is a wrapper function which normalizes the result. The if-else is needed not to compute the normalization
+    for each z, but only once for each zbin_idx"""
+
     if type(z) == float or type(z) == int:
         return niz_unnormalized(z, i, pph) / niz_normalization(i, niz_unnormalized, pph)
 
     elif type(z) == np.ndarray:
         niz_unnormalized_arr = np.asarray([niz_unnormalized(z_value, i, pph) for z_value in z])
         return niz_unnormalized_arr / niz_normalization(i, niz_unnormalized, pph)
+
+    else:
+        raise TypeError('z must be a float, an int or a numpy array')
 
 
 def niz_unnorm_stef(z, i):
@@ -260,23 +225,16 @@ def niz_unnorm_stef(z, i):
     return result
 
 
-niz_normalization_arr = np.asarray([niz_normalization(i, niz_unnormalized, pph_fid) for i in range(zbins)])
-
-z_num = 200
+z_num = 300
 z_grid = np.linspace(z_min, z_max, z_num)
 zbin_idx = 1
 
-niz_fid_list = [niz_unnormalized(z, zbin_idx, pph_fid) for z in z_grid]
-niz_unnorm_list = [niz_unnormalized(z, zbin_idx, pph_tot) for z in z_grid]
+niz_fid = niz_normalized(z_grid, zbin_idx, pph_fid)
+niz_tot = niz_normalized(z_grid, zbin_idx, pph_tot)
 
-niz_norm_pphfid_list = [normalize_niz(z, zbin_idx, niz_unnormalized, pph_fid, niz_normalization_arr) for z in z_grid]
-# niz_norm_pphtot_list = [normalize_niz(z, zbin_idx, niz_unnormalized, pph_tot) for z in z_grid]
-
-niz_normalized_new_test = niz_normalized_new(z_grid, zbin_idx, pph_fid)
-np.allclose(niz_normalized_new_test, niz_norm_pphfid_list, atol=0, rtol=0.00000001)
 plt.figure()
-plt.plot(z_grid, niz_norm_pphfid_list, label='niz_norm_pphfid_list')
-plt.plot(z_grid, niz_normalized_new_test, label='niz_normalized_new_test')
+plt.plot(z_grid, niz_fid, label='niz_fid')
+plt.plot(z_grid, niz_tot, label='niz_tot')
 plt.legend()
 
 assert 1 > 2
